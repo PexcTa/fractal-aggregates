baabe01fb4bdeb9d38559130318b1e1b421409a1
PexcTa <borisvokramar@gmail.com>
2025-12-09 15:43:42 +0300

    reset: start fresh

--- test.py
+++
@@ -1,9 +0,0 @@
-from src.fractal_generator import FractalAggregate
-
-# Create an instance
-generator = FractalAggregate(random_seed=123)
-
-# Generate a small aggregate
-particles = generator.generate_single_aggregate(N=10, visualize=False)
-
-print("Setup successful!")
